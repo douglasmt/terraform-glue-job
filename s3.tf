2971625c9daea172787187436d43cb42e4c08cf6
unknown <douglasmaiatome>
2025-07-14 16:16:58 -0300

    Job glue path key

--- s3.tf
+++ s3.tf
@@ -2,3 +2,10 @@ resource "aws_s3_bucket" "glue_scripts" {
   bucket = "glue-job-scripts-${var.project_suffix}"
   force_destroy = true
 }
+
+resource "aws_s3_object" "glue_script" {
+  bucket = aws_s3_bucket.glue_scripts.bucket
+  key    = "job_script.py"
+  source = "${path.module}/script/job_script.py"
+  etag   = filemd5("${path.module}/script/job_script.py")
+}
