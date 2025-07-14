74980ed844948f25d908a510957e61d9653bcf28
unknown <douglasmaiatome>
2025-07-14 16:36:03 -0300

    Recolocando role

--- iam.tf
+++ iam.tf
@@ -8,15 +8,15 @@ data "aws_iam_policy_document" "glue_assume_role" {
   }
 }
 
-# resource "aws_iam_role" "glue_service_role" {
-#   name = "glue_service_role_${var.project_suffix}"
-#   assume_role_policy = data.aws_iam_policy_document.glue_assume_role.json
-# }
+resource "aws_iam_role" "glue_service_role" {
+  name = "glue_service_role_${var.project_suffix}"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_role.json
+}
 
-# resource "aws_iam_role_policy_attachment" "glue_service_policy" {
-#   role       = aws_iam_role.glue_service_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-# }
+resource "aws_iam_role_policy_attachment" "glue_service_policy" {
+  role       = aws_iam_role.glue_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
 
 # Não criamos a role, apenas anexamos permissão ao grupo existente
 resource "aws_iam_group_policy_attachment" "glue_group_policy" {
