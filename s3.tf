df6dd5ace8b071ba2a0c9155ac50b4d3d370cce0
unknown <douglasmaiatome>
2025-07-14 16:25:29 -0300

    Atualização de bucket

--- s3.tf
+++ s3.tf
@@ -1,10 +1,9 @@
-resource "aws_s3_bucket" "glue_scripts" {
-  bucket = "glue-job-scripts-${var.project_suffix}"
-  force_destroy = true
+data "aws_s3_bucket" "glue_scripts" {
+  bucket = "glue-job-scripts-doug-proj-1"
 }
 
 resource "aws_s3_object" "glue_script" {
-  bucket = aws_s3_bucket.glue_scripts.bucket
+  bucket = data.aws_s3_bucket.glue_scripts.id
   key    = "job_script.py"
   source = "${path.module}/script/job_script.py"
   etag   = filemd5("${path.module}/script/job_script.py")
