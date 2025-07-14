633f131b883df8fa1447c9312c44b605021f0fa1
unknown <douglasmaiatome>
2025-07-14 16:23:03 -0300

    glue service role retirada

--- iam.tf
+++ iam.tf
@@ -13,10 +13,10 @@ data "aws_iam_policy_document" "glue_assume_role" {
 #   assume_role_policy = data.aws_iam_policy_document.glue_assume_role.json
 # }
 
-resource "aws_iam_role_policy_attachment" "glue_service_policy" {
-  role       = aws_iam_role.glue_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-}
+# resource "aws_iam_role_policy_attachment" "glue_service_policy" {
+#   role       = aws_iam_role.glue_service_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+# }
 
 # Não criamos a role, apenas anexamos permissão ao grupo existente
 resource "aws_iam_group_policy_attachment" "glue_group_policy" {
