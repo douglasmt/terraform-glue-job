8fda9a2aa02019877cf996a3134fbb3cc26e1dd0
unknown <douglasmaiatome>
2025-07-14 16:10:11 -0300

    Recomeçando o pipeline

--- glue_job.tf
+++ glue_job.tf
@@ -1,10 +1,10 @@
 resource "aws_glue_job" "mock_job" {
   name     = "job-transform-test"
-  role_arn = "arn:aws:iam::086720243261:role/glue_service_role_doug-proj-2"
+  role_arn = "arn:aws:iam::086720243261:role/glue_service_role_doug-proj-1"
 
   command {
     name            = "glueetl"
-    script_location = "s3://aws-glue-assets-086720243261-us-east-1/scripts/job_script.py"
+    script_location = "s3://${aws_s3_bucket.glue_scripts.bucket}/job_script.py"
     python_version  = "3"
   }
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "project_suffix" {
   description = "Suffix to make resource names unique"
   type        = string
-  default     = "doug-proj-2"
+  default     = "doug-proj-1"
 }
